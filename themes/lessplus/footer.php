54191be61c511142ddcc51b178ad2852a68eaab1
YONG JI <bumbermin@gmail.com>
2016-11-12 15:37:12 +0900

    remove if statement is_home()

--- themes/lessplus/footer.php
+++ themes/lessplus/footer.php
@@ -8,14 +8,12 @@
 	<div class="site-info container">
 		<?php do_action( 'break_credits' ); ?>
 		<!-- Your share button code -->
-		<?php if(is_home() || is_archive()): ?>
-			<div class="soc-buttons">
-				<a target="_blank" href="//twitter.com/intent/tweet?text=<?php echo get_bloginfo('description'); ?>+@ruucm+<?php echo get_bloginfo('url'); ?>" class="soc-buttons-twitter"></a>
-				<a class="soc-buttons-facebook fb-share">
-				</a>
-			</div>
-			<div id="fb-root"></div>
-		<?php endif; ?>
+		<div class="soc-buttons">
+			<a target="_blank" href="//twitter.com/intent/tweet?text=<?php echo get_bloginfo('description'); ?>+@ruucm+<?php echo get_bloginfo('url'); ?>" class="soc-buttons-twitter"></a>
+			<a class="soc-buttons-facebook fb-share">
+			</a>
+		</div>
+		<div id="fb-root"></div>
 	</div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 <?php wp_footer(); ?>
