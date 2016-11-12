f5c1be50cc0f8b0f6fccb9686b287f8bb1257c62
YONG JI <bumbermin@gmail.com>
2016-11-12 13:04:23 +0900

    change to upper registering jquery

--- themes/lessplus/functions.php
+++ themes/lessplus/functions.php
@@ -22,19 +22,15 @@ register_nav_menus(
 /* Enque Styles and Scripts
 /*-----------------------------------------------------------------------------------*/
 
-function less_scripts()  { 
+function less_scripts()  {
+	wp_deregister_script('jquery' );
+	wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', false, '2.2.4', true );
 
 	// theme styles
 	wp_enqueue_style( 'less-style', get_template_directory_uri() . '/css/style.css', '10000', 'all' );
-			
 	// add fitvid
 	wp_enqueue_script( 'less-fitvid', get_template_directory_uri() . '/js/jquery.fitvids.js', array( 'jquery' ), LESS_VERSION, true );
-	
 	// add theme scripts
 	wp_enqueue_script( 'less', get_template_directory_uri() . '/js/theme.min.js', array(), LESS_VERSION, true );
-
-	wp_deregister_script('jquery' );
-	wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', false, '2.2.4', true );
-  
 }
 add_action( 'wp_enqueue_scripts', 'less_scripts' );
